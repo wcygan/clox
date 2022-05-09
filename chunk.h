9c32a7c30cbc0b30053a789508311f220a29ccf2
Will Cygan <wcygan.io@gmail.com>
2022-05-08 21:57:18 -0500

    14.4

--- chunk.h
+++ chunk.h
@@ -2,13 +2,20 @@
 #define CLOX_CHUNK_H
 
 #include "common.h"
+#include "memory.h"
 
 typedef enum {
     OP_RETURN
 } OpCode;
 
 typedef struct {
+    int count;
+    int capacity;
     uint8_t * code;
 } Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
 
 #endif //CLOX_CHUNK_H
--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include "common.h"
 #include <stdio.h>
+#include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
-    printf("Hello Beautiful World!\n");
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
     return 0;
 }
