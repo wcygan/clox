2c77d257b71a43762236d5b61b089cfbb56c95d8
Will Cygan <wcygan.io@gmail.com>
2022-05-10 20:52:49 -0500

    15

--- chunk.h
+++ chunk.h
@@ -7,7 +7,12 @@
 
 typedef enum {
     OP_RETURN,
-    OP_CONSTANT
+    OP_CONSTANT,
+    OP_NEGATE,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
 } OpCode;
 
 typedef struct {
--- vm.c
+++ vm.c
@@ -12,6 +12,13 @@ static void resetStack() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -34,6 +41,26 @@ static InterpretResult run() {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
                 push(constant);
+                break;
+            }
+            case OP_NEGATE: {
+                push(-pop());
+                break;
+            }
+            case OP_ADD: {
+                BINARY_OP(+);
+                break;
+            }
+            case OP_SUBTRACT: {
+                BINARY_OP(-);
+                break;
+            }
+            case OP_MULTIPLY: {
+                BINARY_OP(*);
+                break;
+            }
+            case OP_DIVIDE: {
+                BINARY_OP(/);
                 break;
             }
         }
