f120ac208589bc3ec6d714d3800a067713bfdfb9
Will Cygan <wcygan.io@gmail.com>
2022-05-15 19:13:45 -0500

    20.4

--- table.h
+++ table.h
@@ -17,5 +17,10 @@ typedef struct {
 
 void initTable(Table *table);
 void freeTable(Table *table);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table *from, Table *to);
+
 
 #endif //CLOX_TABLE_H
