68746cd50e10f82d0ced257f9a1758bf3a56bd29
Will Cygan <wcygan.io@gmail.com>
2022-05-18 21:29:02 -0500

    still debugging.. done with 21...

--- debug.c
+++ debug.c
@@ -61,6 +61,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
             return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
         case OP_GET_GLOBAL:
             return constantInstruction("OP_GET_GLOBAL", chunk, offset);
+        case OP_SET_GLOBAL:
+            return constantInstruction("OP_SET_GLOBAL", chunk, offset);
         case OP_POP:
             return simpleInstruction("OP_POP", offset);
         case OP_EQUAL:
--- vm.c
+++ vm.c
@@ -27,9 +27,9 @@ static void runtimeError(const char *format, ...) {
 }
 
 static InterpretResult run() {
-#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
     do { \
       if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
@@ -91,27 +91,35 @@ static InterpretResult run() {
             case OP_POP:
                 pop();
                 break;
-            case OP_EQUAL: {
-                Value b = pop();
-                Value a = pop();
-                push(BOOL_VAL(valuesEqual(a, b)));
-                break;
-            }
             case OP_GET_GLOBAL: {
-                ObjString *name = READ_STRING();
+                ObjString* name = READ_STRING();
                 Value value;
                 if (!tableGet(&vm.globals, name, &value)) {
                     runtimeError("Undefined variable '%s'.", name->chars);
                     return INTERPRET_RUNTIME_ERROR;
                 }
-
                 push(value);
+                break;
+            }
+            case OP_SET_GLOBAL: {
+                ObjString* name = READ_STRING();
+                if (tableSet(&vm.globals, name, peek(0))) {
+                    tableDelete(&vm.globals, name);
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
                 break;
             }
             case OP_DEFINE_GLOBAL: {
                 ObjString* name = READ_STRING();
                 tableSet(&vm.globals, name, peek(0));
                 pop();
+                break;
+            }
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(valuesEqual(a, b)));
                 break;
             }
             case OP_GREATER:
@@ -144,10 +152,10 @@ static InterpretResult run() {
                 break;
         }
     }
-
+#undef READ_CONSTANT
 #undef READ_STRING
 #undef READ_BYTE
-#undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 void initVM() {
